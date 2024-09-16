ef1bcb827d3bdaa44cff7d6e6afdf202d8c51ac7
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2024-09-16 20:11:35 -0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let tourist = 'Jonh'
-let citiesNum = 0
+let tourist = prompt('Qual o nome do turista')
+let visited = prompt('Você visitou alguma cidade?')
 let citiesNom = ''
-let visited = true
-while (visited == true) {
-    citiesNum += 3
-    citiesNom += [`\nSão Paulo \nRio de Janeiro \nFortaleza`]
-    visited = false
-    break
+let citiesNum = 0
+while (visited === 'sim') {
+    citiesNum++
+    citiesNom = (citiesNom + ' ' + prompt('Qual o nome da cidade?'))
+    visited = prompt('Você visitou mais alguma cidade?')
 }
-console.log(`Turista : ${tourist}\nQuantidade de cidades visitadas : ${citiesNum}
-Cidades visitadas : ${citiesNom}`)
+alert(` Turista: ${tourist}\n Quantidade de cidades visitadas: ${citiesNum}\nCidades visitadas: ${citiesNom} `)
